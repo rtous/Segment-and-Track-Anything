d1d422ab5782f86fc28041bb3b4d4d9595b89dc7
Ruben Tous <ruben.tous@adsmurai.com>
2023-10-03 07:54:09 +0200

    nc

---
+++ ruben.py
@@ -0,0 +1,76 @@
+from PIL.ImageOps import colorize, scale
+import gradio as gr
+import importlib
+import sys
+import os
+import pdb
+from matplotlib.pyplot import step
+
+from model_args import segtracker_args,sam_args,aot_args
+from SegTracker import SegTracker
+from tool.transfer_tools import draw_outline, draw_points
+# sys.path.append('.')
+# sys.path.append('..')
+
+import cv2
+from PIL import Image
+from skimage.morphology.binary import binary_dilation
+import argparse
+import torch
+import time, math
+from seg_track_anything import aot_model2ckpt, tracking_objects_in_video, draw_mask
+import gc
+import numpy as np
+import json
+from tool.transfer_tools import mask2bbox
+
+def main():
+
+	file_path = "data/scenes/tiktok2/imagesFull"
+	imgs_path = sorted([os.path.join(file_path, img_name) for img_name in os.listdir(file_path)])
+    first_frame = imgs_path[0]
+    first_frame = cv2.imread(first_frame)
+    first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB)
+
+	origin_frame = first_frame
+	aot_model = "r50_deaotl"# "deaotb", "deaotl", "r50_deaotl" (default "r50_deaotl")                                 
+	long_term_mem = 9999 #1-9999 (default 9999)
+	max_len_long_term = 9999 #1-9999 (default 9999)
+	sam_gap = 100 #1-9999 (default 100)
+	max_obj_num = 50 #50-300 (default 255)
+	points_per_side = 16 #1-100 (default 16)
+
+	Seg_Tracker, _, _, _ = init_SegTracker(aot_model, long_term_mem, max_len_long_term, sam_gap, max_obj_num, points_per_side, origin_frame)
+
+def init_SegTracker(aot_model, long_term_mem, max_len_long_term, sam_gap, max_obj_num, points_per_side, origin_frame):
+    
+    if origin_frame is None:
+        return None, origin_frame, [[], []], ""
+
+    # reset aot args
+    aot_args["model"] = aot_model
+    aot_args["model_path"] = aot_model2ckpt[aot_model]
+    aot_args["long_term_mem_gap"] = long_term_mem
+    aot_args["max_len_long_term"] = max_len_long_term
+    # reset sam args
+    segtracker_args["sam_gap"] = sam_gap
+    segtracker_args["max_obj_num"] = max_obj_num
+    sam_args["generator_args"]["points_per_side"] = points_per_side
+    
+    Seg_Tracker = SegTracker(segtracker_args, sam_args, aot_args)
+    Seg_Tracker.restart_tracker()
+
+    return Seg_Tracker, origin_frame, [[], []], ""
+
+def SegTracker_add_first_frame(Seg_Tracker, origin_frame, predicted_mask):
+    with torch.cuda.amp.autocast():
+        # Reset the first frame's mask
+        frame_idx = 0
+        Seg_Tracker.restart_tracker()
+        Seg_Tracker.add_reference(origin_frame, predicted_mask, frame_idx)
+        Seg_Tracker.first_frame_mask = predicted_mask
+
+    return Seg_Tracker
+
+if __name__ == "__main__":
+    main()
