219580a07928a6ad7816fe2b02a31c0472bac779
Ruben Tous <ruben.tous@adsmurai.com>
2023-10-09 12:59:07 +0200

    ruben

--- ruben.py
+++ ruben.py
@@ -50,7 +50,7 @@ def main():
 	#grounding_caption = "legs, skin, tshirt, ball, hair" FAIL
 	#grounding_caption = "skin, legs, arms, tshirt, ball, hair" #OK NO HAIR
 	#grounding_caption = "face, hair, arms, legs, tshirt, ball"
-	grounding_caption = "body, arms, hair" 
+	grounding_caption = "ball, body, arms, hair" 
 	#########################
 	
 
